ab0e0467a6dea28de121c8127d493a7cec4153aa
ives <mine@ives.pro>
2022-06-30 13:40:58 +0800

    聊聊集成测试和e2e测试

--- src/cache-users/cache-users.test.ts
+++ src/cache-users/cache-users.test.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { fetchUsers, filterValidUsers, storageUsers } from "./cache-users";
+import * as cacheUsersModule from './cache-users';
+import { cacheUsers, fetchUsers, filterValidUsers, storageUsers } from "./cache-users";
 import { User } from "./domains";
 
 jest.mock('axios');
@@ -7,10 +8,18 @@ jest.mock('axios');
 
 describe('cacheUsers test', function () {
   
+  beforeAll(() => {
+    global.localStorage = {
+      setItem(_: string, __: string) {
+      }
+    } as Storage;
+  });
+  
+  const getAxiosGetMock = () => (axios.get as unknown as jest.Mock);
+  
   describe('fetchUsers test', function () {
-  
-    const getAxiosGetMock = () => (axios.get as unknown as jest.Mock);
-  
+    
+    
     it('should throw error when get api error', function () {
       getAxiosGetMock()
         .mockRejectedValueOnce('');
@@ -67,31 +76,62 @@ describe('cacheUsers test', function () {
         { id: 2, name: "2", status: 2 },
         { id: 3, name: "3", status: 2 }
       ];
-      const exceptData:User[] = [
-        { id: 1, name: "1", status: 1 },
-      ]
-  
+      const exceptData: User[] = [
+        { id: 1, name: "1", status: 1 }
+      ];
+      
       expect(filterValidUsers(mockData)).toMatchObject(exceptData);
     });
   });
   
   describe('storageUsers test', function () {
-    beforeAll(() => {
-      global.localStorage = {
-        setItem(_: string, __: string) {
-        }
-      } as Storage
-    })
+    
     it('should called localStorage.setItem with right params', function () {
-      const spy = jest.spyOn(global.localStorage,'setItem');
+      const spy = jest.spyOn(global.localStorage, 'setItem');
       
       const mockData: User[] = [
         { id: 1, name: "1", status: 1 },
         { id: 2, name: "2", status: 1 },
         { id: 3, name: "3", status: 1 }
       ];
-      storageUsers(mockData)
-      expect(spy).toHaveBeenNthCalledWith(1,'users',JSON.stringify(mockData));
+      storageUsers(mockData);
+      expect(spy).toHaveBeenNthCalledWith(1, 'users', JSON.stringify(mockData));
+    });
+    
+  });
+  
+  describe('cache test by order', function () {
+    
+    let fetchUsersSpy: jest.SpyInstance;
+    let filterUsersSpy: jest.SpyInstance;
+    
+    beforeEach(async () => {
+      getAxiosGetMock().mockResolvedValue({ data: [] });
+      fetchUsersSpy = jest.spyOn(cacheUsersModule, 'fetchUsers');
+      filterUsersSpy = jest.spyOn(cacheUsersModule, 'filterValidUsers');
+    });
+    
+    it('should called fetchUsers', async function () {
+      await cacheUsers();
+      expect(fetchUsersSpy).toHaveBeenCalledTimes(1);
+      fetchUsersSpy.mockClear();
+    });
+    
+    it('should called filter with empty array', async function () {
+      fetchUsersSpy.mockResolvedValueOnce([]);
+      await cacheUsers();
+      expect(filterUsersSpy).toHaveBeenCalledWith([]);
+      filterUsersSpy.mockClear();
+    });
+    
+    it('should called storageUsersSpy at last time', async function () {
+      const mockData:User[] = [ { id: 1, name: "12", status: 1 } ];
+      const storageUsersSpy = jest.spyOn(cacheUsersModule, 'storageUsers');
+      filterUsersSpy.mockReturnValue(mockData)
+      await cacheUsers();
+      expect(storageUsersSpy).toHaveBeenCalledWith(
+        mockData
+      );
     });
     
   });
